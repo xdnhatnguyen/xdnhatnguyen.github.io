24dd306e623cd859d4329ad001901d3e496fd964
xdnhatnguyen <nhat33219@gmail.com>
2026-08-09 10:06:11 +0000

    Deploying to gh-pages from @ xdnhatnguyen/xdnhatnguyen.github.io@565c1b2275d9026d010bdc4e65cf8a4b7c2c33b4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-literature-review-stylegallery-cvpr-2026",
+        },{id: "post-project-report-style-transfer-visualization",
+        
+          title: "Project Report: Style Transfer Visualization",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/style-transfer/";
+          
+        },
+      },{id: "post-literature-review-stylegallery-cvpr-2026",
         
           title: "Literature Review: StyleGallery (CVPR 2026)",
         
